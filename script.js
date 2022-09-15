40d94d76bfb10f3be8814ee96fe5a85fcd32060d
Rifki <mrifkipratama@gmail.com>
2022-09-15 20:30:12 +0700

    feat : display List of Carsa

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const startFetchDataBtn = document.getElementById("startFetchDataBtn")
+const rentCarsList = document.getElementById("rentCarsList")
 let rentedCarsData = []
 
-startFetchDataBtn.addEventListener("click", fetchRentCarsData)
+startFetchDataBtn.addEventListener("click", fetchRentCarsDataAsyncAwait);
 
 
 
@@ -21,8 +22,39 @@ async function fetchRentCarsDataAsyncAwait() {
         let rentCars = await result.json()
 
         rentedCarsData = rentCars
+
+        const first10Cars = rentedCarsData.slice(0, 10)
+
+        first10Cars.forEach((car) => {
+            rentCarsList.append(createCarCard(car))
+
+        })
+
     } catch (err) {
         console.error(err)
     }
 
+}
+
+function createCarCard(car) {
+    const carCard = document.createElement('div')
+    carCard.setAttribute('class', 'carCard')
+
+    const carImage = document.createElement('img')
+    carCard.setAttribute('src', 'carImage')
+    carCard.setAttribute('src', avanza.image)
+
+    const carName = document.createElement('p')
+    carCard.setAttribute('class', 'carName')
+    carName.innerText = avanza.name
+
+    const carPrice = document.createElement('p')
+    carPrice.setAttribute('class', 'carPrice')
+    carPrice.innerText = avanza.price
+
+    carCard.append(carImage)
+    carCard.append(carName)
+    carCard.append(carPrice)
+
+    rentCarsList.append(carCard)
 }
